9f113b295ee9fd1865f16497d7f933035008713e
Matt The King Jenkins <Jenkinsmj2@vcu.edu>
2014-09-06 18:24:20 -0400

    Added Realtor Route
    added realtor route to realtalk.js

--- realtalk.js
+++ realtalk.js
@@ -3,11 +3,17 @@ if (Meteor.isClient) {
     routes: {
       ""        : "main",
       "index"   : "main",
-      "Realtor" : "main"
+      "Realtor" : "Realtor"
     },
     main: function(page) {
       document.body.innerHTML = "";
       page = page?page:"index";
+      UI.insert(UI.render(Template[page]), document.body);
+      document.body.style.padding = "0px 0px 0px 0px";
+    },
+    Realtor: function(page) {
+      document.body.innerHTML = "";
+      page = page?page:"Realtor";
       UI.insert(UI.render(Template[page]), document.body);
       document.body.style.padding = "0px 0px 0px 0px";
     }
